bc32c920b13deb61dc37e095c6b54e73b3099e76
Miika Pernu <dorvenaattori@gmail.com>
2018-09-22 14:30:19 +0300

    Fixed block decryption & substitution

--- des.py
+++ des.py
@@ -41,11 +41,13 @@ class DES:
         
         self._initial_permutation = [58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7]
         
+        self._permutation_final = [40,8,48,16,56,24,64,32,39,7,47,15,55,23,63,31,38,6,46,14,54,22,62,30,37,5,45,13,53,21,61,29,36,4,44,12,52,20,60,28,35,3,43,11,51,19,59,27,34,2,42,10,50,18,58,26,33,1,41,9,49,17,57,25]
+        
         self._permutation = [16,7,20,21,29,12,28,17,1,15,23,26,5,18,31,10,2,8,24,14,32,27,3,9,19,13,30,6,22,11,4,25]
         
         self._round_keys = list()
         
-        self._substitution_boxes = [   [[4,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7],
+        self._substitution_boxes = [   [[14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7],
                                         [0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8],
                                         [4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0],
                                         [15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13]],
@@ -91,34 +93,37 @@ class DES:
     def _encryptBlock(self, data_64bit, round_keys):
         data_64bit = self._permute(data_64bit, self._initial_permutation, 64)
         
+        print("IP: " + str(data_64bit))
+        
         #Split the data
         left_side = (data_64bit & 0xFFFFFFFF00000000) >> 32
         right_side = (data_64bit & 0x00000000FFFFFFFF)
         
         for i in range(0,16):
+            print("Cipher round " + str(i))
             cipher_round_output = self._cipherRound(left_side, right_side, round_keys[i])
             
             left_side = cipher_round_output[0]
             right_side = cipher_round_output[1]
             
-        return ((left_side << 32) | right_side)
+            print("Left 1 value: " + str(left_side))
+            print("Right 1 value " + str(right_side))
+            
+            print((left_side | (right_side << 32)))
+            
+        return self._permute((left_side | (right_side << 32)),self._permutation_final, 64)
     
     def _cipherRound(self, left0_32bit, right0_32bit, round_key):
-        print("Starting a new cipher round.\n")
         
         print("Left 0 value: " + str(left0_32bit))
         print("Right 0 value: " + str(right0_32bit))
+        print("Round key: " + str(round_key))
         
         print("")
         
         left1_32bit = right0_32bit
        
-        right1_32bit = self._cipherFunction(right0_32bit, round_key) ^ left0_32bit
-        
-        print("Left 1 value: " + str(left1_32bit))
-        print("Right 1 value " + str(right1_32bit))
-        
-        print("")
+        right1_32bit = (self._cipherFunction(right0_32bit, round_key) ^ left0_32bit)
         
         return left1_32bit, right1_32bit
         
@@ -126,19 +131,27 @@ class DES:
     def _cipherFunction(self, data_32bit, round_key):
         #perform expansion permutation (32bit to 48bit)
         data_48bit = self._permute(data_32bit, self._expansion, 32)
+        print("Expansion " + str(data_48bit))
         #Xor value with the round key of iteration
         xor_value = data_48bit ^ round_key
+        print("Xor : " + str(xor_value))
         #perform substitution
         substitution = self._substitute(xor_value)
+        print("Substitutino: " + str(substitution))
         #Permutation
-        return self._permute(substitution, self._permutation, 48)
+        return self._permute(substitution, self._permutation, 32)
     
     def _substitute(self, xor_value):        
         output_data = 0
         
+        print("XOR VALUE : " + str(xor_value))
+        
         #break 48bit xor value into 8 x 6 bit blocks        
         for i in range(0, 8):
-            block = (xor_value & (0b111111 << (i * 6))) >> i * 6
+            shift_amount = 42 - (i * 6)
+            
+            block = ((xor_value & (0b111111 << shift_amount)) >> shift_amount)
+            print("Block : " + str(block))
 
             block_outer_bits = block & 0b100001
             
@@ -147,15 +160,9 @@ class DES:
             
             block_inner_bits = (block & 0b011110) >> 1  #shifted left to get directly the correct index in substitution table (column)
             
-            #print("Outer bits: " + str(block_outer_bits))
-            #print("Inner bits: " + str(block_inner_bits))
-            
             substituted_block = self._substitution_boxes[i][block_outer_bits][block_inner_bits]
-            
-            #print(substituted_block)
-            
             #Put the substituted block back into the original place in the input
-            output_data = output_data | ((substituted_block) << i * 6 )
+            output_data = output_data | ((substituted_block) << (28 - (i * 4)))
             
         return output_data
             
@@ -163,24 +170,15 @@ class DES:
     def _createRoundkeys(self, key_64bit ):        
         #FIRST PERMUTATION        
         key_64bit = self._permute(key_64bit, self._pc_table1, 64)
-        
-        #print("ROUND KEY CREATION")
-        #print("Original key: " + str(key_64bit))
 
         left_side =     key_64bit >> 28
         right_side =    (key_64bit & 0xFFFFFFF)
-        
-        #print("Left side: " + str(left_side))
-        #print("Right side: " + str(right_side))
         
         for i in range(0,16):
             #Shift for both of the sides
             left_side = self._leftShift(left_side, 28, self._roundkey_shift[i])
             right_side = self._leftShift(right_side, 28, self._roundkey_shift[i])
             
-            #print("left shifted: " + str(left_side))
-            #print("right shifted: " + str(right_side))
-
             #Combine sides
             combined_sides = ((left_side << 28) | right_side)
                         
@@ -214,7 +212,6 @@ class DES:
             return 0
             
     def _leftShift(self, value, size_bits, num_shifts):
-        
         #No reason to shift more than once around 
         num_shifts = num_shifts % size_bits
         
